8f85a353b29b520d353d2faa8e71c3aa14782f1e
IxXSnowFallXxI <korneev.e.s.95@gmail.com>
2025-12-05 16:58:47 +0400

    Fully functional game

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,14 +1,20 @@
 import tkinter as tk
-from tkinter import PhotoImage
+from tkinter import PhotoImage, messagebox
 root = tk.Tk()
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 475
+HEIGHT = 520
 root.geometry(f'{WIDTH}x{HEIGHT}')
 root.title('Кресики-нолики')
 icon = PhotoImage(file='/home/yegor/PythonFiles/Projects/TicTac/image.png')
 root.iconphoto(False, icon)
 buttons = []
 current_player = "X"
+
+def reset_game():
+    global current_player
+    current_player = "X"
+    for button in buttons:
+        button.config(text="", bg='grey',fg='white')
 
 def check_winner():
     winning_combinations = [
@@ -19,6 +25,9 @@ def check_winner():
     for comb in winning_combinations:
         a, b, c = comb
         if buttons[a]["text"]  == buttons[b]["text"] == buttons[c]["text"]  != "":
+            buttons[a].config(bg='lightgreen', fg='blue')
+            buttons[b].config(bg='lightgreen', fg='blue')
+            buttons[c].config(bg='lightgreen', fg='blue')
             return True
     return False
 
@@ -29,23 +38,32 @@ def on_click(index):
 
         if check_winner():
             if current_player == "X":
-                print("Победили крестики")
+                messagebox.showinfo("Победа!", "Победили крестики!")
             else:
-                print("Победили нолики")
-        if current_player == "X":
-            current_player = "O"
+                messagebox.showinfo("Победа!", "Победили нолики!")
+        elif all(button['text'] != "" for button in buttons):
+            for button in buttons:
+                button.config(bg='red')
+            messagebox.showinfo("", "Ничья!")
         else:
-            current_player = "X"
+            if current_player == "X":
+                current_player = "O"
+            else:
+                current_player = "X"
         
 for i in range(9):
     button = tk.Button(
         root,
         text="",
         font = ('Arial', 14),
+        bg='grey',
+        fg='white',
         width=13,
         height=7,
         command=lambda idx=i: on_click(idx),
         )
     button.grid(row=i//3, column=i%3)
     buttons.append(button)
+reset_button=tk.Button(root, text='Новая игра', font=('Arial', 14), command=reset_game)
+reset_button.grid(row=3, column=0, columnspan=3, sticky="we")
 root.mainloop()
